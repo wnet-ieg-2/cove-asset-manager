53230cd308ce0b3009f6ebdb1d8e0de8f1e4940f
William Tam <tamw@wnet.org>
2017-03-13 11:03:32 -0400

    Adding media manager endpoint setting field

--- classes/class-cove-asset-manager-settings.php
+++ classes/class-cove-asset-manager-settings.php
@@ -103,6 +103,8 @@ class COVE_Asset_Manager_Settings {
     add_settings_field( 'coveam_mm_api_secret' , 'PBS Media Manager API secret' , array( $this , 'settings_field' )  , 'cove_asset_manager_settings' , 'main_settings' , array('coveam_mm_api_secret', 'Get from PBS') );
     register_setting( 'cove_asset_manager_settings' , 'coveam_mm_api_secret' );
 
+    add_settings_field( 'coveam_mm_api_endpoint' , 'PBS Media Manager API endpoint' , array( $this , 'settings_field' )  , 'cove_asset_manager_settings' , 'main_settings' , array('coveam_mm_api_endpoint', 'Staging: https://media-staging.services.pbs.org/api/v1 Prod: https://media.services.pbs.org/api/v1') );
+    register_setting( 'cove_asset_manager_settings' , 'coveam_mm_api_endpoint' );
 
 
 

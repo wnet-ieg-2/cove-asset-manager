969574d9a0b4be6a81545f63f73d48de96740fa4
IEG WNET <wongb@13.org>
2017-04-12 16:08:01 -0400

    re-adding in the character count validator

--- assets/js/media_manager_admin.js
+++ assets/js/media_manager_admin.js
@@ -12,5 +12,26 @@ jQuery(document).ready(function($) {
 		}
 		e.preventDefault();
 	});
-	
+  // validator taken from http://stackoverflow.com/users/352705/tbleckert
+  $("textarea[data-limit-input], input[data-limit-input]").keyup(function (e) {
+    var $this      = $(this),
+        charLength = $this.val().length,
+        charLimit  = $this.attr("data-limit-input");
+        //Displays count
+    $this.next("span").html(charLength + " of " + charLimit + " characters used");
+    // Alert when max is reached
+    if ($this.val().length > charLimit) {
+      $this.next("span").html("<strong>You may only have up to " + charLimit + " characters.</strong>");
+    }
+  });
+
+  $("textarea[data-limit-input], input[data-limit-input]").keydown(function (e) {
+    var $this      = $(this),
+        charLength = $this.val().length,
+        charLimit  = $this.attr("data-limit-input");
+
+    if ($this.val().length > charLimit && e.keyCode !== 8 && e.keyCode !== 46) {
+      return false;
+    }
+  });
 });

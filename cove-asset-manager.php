b7090f0493292e52e5747c5229bdd33e72969bda
William Tam <tamw@wnet.org>
2020-02-19 17:04:28 +0000

    removing extraneous error logging

--- cove-asset-manager.php
+++ cove-asset-manager.php
@@ -438,7 +438,6 @@ function coveam_daily_expire_videos() {
 function coveam_check_inprogress_ingest_videos() {
     $plugin_obj = new COVE_Asset_Manager( __FILE__ );
     $ignore_vids_before_this_date = date("Y-m-d", strtotime("-3 day", time()));
-    error_log("ignoring vids before $ignore_vids_before_this_date");
     $inprogress_videos = array();
 
     $allowed_post_types = get_option('coveam_showonposttypes');

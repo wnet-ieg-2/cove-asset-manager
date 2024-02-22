e463a814dee4991b0a57ac36b53141194e80341b
William Tam <tamw@wnet.org>
2024-02-22 15:00:42 -0500

    fix fatal error with youtube sideloader

--- classes/class-youtube-oauth.php
+++ classes/class-youtube-oauth.php
@@ -243,10 +243,10 @@ class WNET_Google_oAuth {
     preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $url, $matches);
     $file_array['name'] = basename($matches[0]);
     $file_array['tmp_name'] = $tmp;
-
+    error_log(json_encode($tmp)); 
     // If error storing temporarily, unlink
     if ( is_wp_error( $tmp ) ) {
-      $tmp_name = __toString($tmp);
+      $tmp_name = (string) $tmp;
       @unlink($tmp_name);
       $file_array['tmp_name'] = '';
     }
